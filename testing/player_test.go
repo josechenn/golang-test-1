c376cb91c4b33338d9f385fc2849087043e14a70
Jose Renaldo Tamdy <jose.renaldo@pricebook.co.id>
2022-01-20 20:38:28 +0700

    add unit test

--- testing/player_test.go
+++ testing/player_test.go
@@ -1,6 +1,8 @@
 package testing
 
 import (
+	"bytes"
+	"mime/multipart"
 	"myapp/service"
 	"net/http"
 	"net/http/httptest"
@@ -20,6 +22,104 @@ func TestGetPlayerByTeamID(t *testing.T) {
 	req.URL.RawQuery = q.Encode()
 	rr := httptest.NewRecorder()
 	handler := http.HandlerFunc(service.ReturnPlayer)
+	handler.ServeHTTP(rr, req)
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+}
+
+func TestGetPlayerByPlayerName(t *testing.T) {
+
+	req, err := http.NewRequest("GET", "/get_player", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	q := req.URL.Query()
+	q.Add("player_name", "ronaldo")
+	req.URL.RawQuery = q.Encode()
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(service.ReturnPlayer)
+	handler.ServeHTTP(rr, req)
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+}
+
+func TestGetPlayerNotFound(t *testing.T) {
+
+	req, err := http.NewRequest("GET", "/get_player", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	q := req.URL.Query()
+	q.Add("player_name", "testing")
+	req.URL.RawQuery = q.Encode()
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(service.ReturnPlayer)
+	handler.ServeHTTP(rr, req)
+	if status := rr.Code; status != http.StatusNotFound {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+}
+
+func TestGetAllPlayer(t *testing.T) {
+
+	req, err := http.NewRequest("GET", "/get_player", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(service.ReturnPlayer)
+	handler.ServeHTTP(rr, req)
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+}
+
+func TestInsertPlayer(t *testing.T) {
+	body := new(bytes.Buffer)
+	writer := multipart.NewWriter(body)
+	writer.WriteField("player_number", "4")
+	writer.WriteField("player_name", "xyz")
+	writer.WriteField("position", "ST")
+	writer.WriteField("team_id", "1")
+	writer.Close()
+
+	req, err := http.NewRequest("POST", "/insert_player", body)
+	if err != nil {
+		t.Fatal(err)
+	}
+	req.Header.Set("Content-Type", writer.FormDataContentType())
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(service.InsertPlayer)
+	handler.ServeHTTP(rr, req)
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+}
+
+func TestUpdatePlayer(t *testing.T) {
+	body := new(bytes.Buffer)
+	writer := multipart.NewWriter(body)
+	writer.WriteField("player_number", "4")
+	writer.WriteField("player_name", "xyz")
+	writer.WriteField("position", "ST")
+	writer.WriteField("team_id", "1")
+	writer.WriteField("player_id", "7")
+	writer.Close()
+
+	req, err := http.NewRequest("PUT", "/update_player", body)
+	if err != nil {
+		t.Fatal(err)
+	}
+	req.Header.Set("Content-Type", writer.FormDataContentType())
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(service.UpdatePlayer)
 	handler.ServeHTTP(rr, req)
 	if status := rr.Code; status != http.StatusOK {
 		t.Errorf("handler returned wrong status code: got %v want %v",
--- testing/team_test.go
+++ testing/team_test.go
@@ -1,6 +1,8 @@
 package testing
 
 import (
+	"bytes"
+	"mime/multipart"
 	"myapp/service"
 	"net/http"
 	"net/http/httptest"
@@ -20,6 +22,41 @@ func TestGetTeamByID(t *testing.T) {
 	req.URL.RawQuery = q.Encode()
 	rr := httptest.NewRecorder()
 	handler := http.HandlerFunc(service.ReturnPlayer)
+	handler.ServeHTTP(rr, req)
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+}
+
+func TestGetAllTeam(t *testing.T) {
+
+	req, err := http.NewRequest("GET", "/get_all_team", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(service.ReturnPlayer)
+	handler.ServeHTTP(rr, req)
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+}
+
+func TestInsertTeam(t *testing.T) {
+	body := new(bytes.Buffer)
+	writer := multipart.NewWriter(body)
+	writer.WriteField("team_name", "Barcelona")
+	writer.Close()
+
+	req, err := http.NewRequest("POST", "/insert_team", body)
+	if err != nil {
+		t.Fatal(err)
+	}
+	req.Header.Set("Content-Type", writer.FormDataContentType())
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(service.InsertTeam)
 	handler.ServeHTTP(rr, req)
 	if status := rr.Code; status != http.StatusOK {
 		t.Errorf("handler returned wrong status code: got %v want %v",
